4b502dd699320443f68f51ed49d70c0eaf3b268f
abrarradan30 <abradan84@gmail.com>
2023-09-21 20:16:55 +0700

    test function

--- Haskell/myFunction.hs
+++ Haskell/myFunction.hs
@@ -1,3 +1,19 @@
+{-
 f :: Int -> Int
 
 f x = x*x
+
+-}
+-- deklarasi dan definisi fungsi f
+f :: Int -> Int -> Int
+f x y = 3*x + 5*y + 2
+
+-- deklarasi dan definisi fungsi g
+g :: Int -> Int -> Int 
+g x y = x^2 + y^2
+
+-- deklarasi dan definisi fungsi h
+h :: Double -> Double -> Double
+h x y = 16 - (x - 3)^2 - (y - 2)^2
+
+main = print(f 5 4, g 5 3, h 5 2)
