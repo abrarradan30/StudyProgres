4b198a15a7d6db3de4e8fd54f1201ed16fe28a24
abrarradan30 <abradan84@gmail.com>
2023-09-21 22:30:48 +0700

    test code

--- Haskell/test.hs
+++ Haskell/test.hs
@@ -9,6 +9,7 @@ main = do let x = 10
               y = 5
           print(x,y)
 -}
+{- 
 main = do
     if True
        then do 
@@ -16,3 +17,9 @@ main = do
             print(x+5)
        else do 
             print("False")
+-}
+f :: Int -> Int
+f x = (x^2) - 3
+
+g :: Int -> Int
+g x = (2*x) + 5
