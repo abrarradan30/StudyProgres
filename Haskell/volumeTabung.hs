cd4d2315f279a369c76211daa4f07d481a5269f1
abrarradan30 <abradan84@gmail.com>
2023-09-21 20:55:36 +0700

    Calculate the area of the tube

---
+++ Haskell/volumeTabung.hs
@@ -0,0 +1,16 @@
+luasLingkaran :: Double -> Double
+luasLingkaran r = pi*r*r
+ 
+volumeTabung :: Double -> Double -> Double
+volumeTabung r t = luasLingkaran r * t
+
+-- atau 
+{-
+luasLingkaran :: Double -> Double
+luasLingkaran r = do 
+    let nilaiPi = 22/7
+    nilaiPi*r*r
+ 
+volumeTabung :: Double -> Double -> Double
+volumeTabung r t = luasLingkaran r * t
+-}
